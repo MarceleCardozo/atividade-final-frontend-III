acad1166cabbdb671aee8a20d5133971a283a755
MarceleDutra Cardozo <marcelecardozo@gmail.com>
2023-06-22 14:55:02 -0300

    Adicionado evento de escuta para executar a função 'showCharactersOnScreen' quando o DOM for carregado. A função busca os dados dos personagens na API de Rick and Morty, gera cartões HTML para cada personagem e os exibe na tela.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
 const api = axios.create({
   baseURL: "https://rickandmortyapi.com/api",
 });
+
+async function showCharactersOnScreen(ev) {
+  ev.preventDefault();
+
+  try {
+    const characterResponse = await api.get(
+      "https://rickandmortyapi.com/api/character"
+    );
+
+    const characters = characterResponse.data.results;
+
+    const cards = document.querySelector("#container-cards");
+
+    console.log(characters);
+
+    characters.forEach((character) => {
+      cards.innerHTML += `
+      <div class="col-4 p-2">
+        <div class="card border-info mt-5"" style="width: 14rem;">
+        <img src="https://rickandmortyapi.com/api/character/avatar/${character.id}.jpeg"/>
+          <div class="card-body">
+            <h5 class="card-title">${character.name}</h5>
+            <p>
+            ${character.status} - ${character.species}
+            </p>
+            <p class="card-text ">
+            Last known location:
+            <br>
+            <span>${character.location.name}</span>
+            </p>
+            <p class="card-text">
+            Last seen on:
+            <span></span>
+            </p>
+          </div>
+        </div>
+      </div>`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", showCharactersOnScreen);
